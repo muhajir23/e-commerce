ddd6d21ea91e4a9059677529b5cd03b3a15baece
muhajir23 <muhajir.cyber23@gmail.com>
2016-10-04 05:36:33 -0700

    version 1.0

--- src/java/com/muhajir/view/CategoryPakaianView.java
+++ src/java/com/muhajir/view/CategoryPakaianView.java
@@ -32,7 +32,7 @@ public class CategoryPakaianView extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
+            out.println("<!DOCTYPE html5>");
             out.println("<html>");
             out.println("<head>");
             out.println("<title>Servlet CategoryPakaianView</title>");            
